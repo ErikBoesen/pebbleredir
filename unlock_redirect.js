740656f938c96cb425e5a6c1a539b62a9acc6496
ErikBoesen <me@erikboesen.com>
2018-11-08 09:45:58 -0500

    Fix wrong logging and string popping

--- unlock_redirect.js
+++ unlock_redirect.js
@@ -1,8 +1,7 @@
-console.log('Switching to user 1!');
+console.log('Redirecting to Rebble documentation.');
 document.body.innerHTML = 'Redirecting...';
 var loc = window.location.href,
     split_loc = loc.split('/');
 split_loc.splice(2, 0, 'developer.rebble.io');
-split_loc.pop();
 split_loc.push('index.html');
 window.location.href = split_loc.join('/');
