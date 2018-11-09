780ae20de091daf8833870e726cd31e762afbe8f
ErikBoesen <me@erikboesen.com>
2018-11-08 22:35:19 -0500

    Fix double slash at end of redirectUrl

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ chrome.webRequest.onBeforeRequest.addListener(function(frame) {
     console.log(frame);  // debug
     var split_url = frame.url.split('/');
     split_url.splice(2, 0, 'developer.rebble.io');
+    split_url.pop();
     split_url.push('index.html');
     return { redirectUrl: split_url.join('/') };
 },
