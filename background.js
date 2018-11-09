3be75b8ec0d3bf9756877d69f3be12ec21bb8c3c
ErikBoesen <me@erikboesen.com>
2018-11-08 22:29:31 -0500

    Properly check page and block it

--- background.js
+++ background.js
@@ -1,5 +1,37 @@
-chrome.webNavigation.onBeforeNavigate.addListener(function(frame) {
+chrome.webRequest.onBeforeRequest.addListener(function(frame) {
     console.log(frame);  // debug
-    console.log(frame.url);
-    chrome.tabs.update(frame.tabId, {url: 'https://google.com'});
-});
+    return {cancel: true};
+    return {redirectUrl: "https://github.com/"};
+    chrome.tabs.update({url: 'https://google.com'});
+},
+{
+    urls: [
+        "*://developer.getpebble.com/*",
+        "*://developer.pebble.com/*",
+        "*://erikboesen.com/*"
+    ],
+    types: ["main_frame"]
+},
+["blocking"]
+);
+/*const filter = {
+  urls: [
+    '*://news.ycombinator.com/*',
+  ],
+}
+
+const webRequestFlags = [
+  'blocking',
+];
+window.chrome.webRequest.onBeforeRequest.addListener(
+  page => {
+    console.log('page blocked - ' + page.url);
+
+    return {
+      cancel: true,
+    };
+  },
+  filter,
+  webRequestFlags,
+);
+*/
