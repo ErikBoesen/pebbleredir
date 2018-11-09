bd1d28dd44d7f5baf8870a5d60cd77f55fe07565
ErikBoesen <me@erikboesen.com>
2018-11-08 22:35:44 -0500

    Delete old unlock_redirect.js script

--- unlock_redirect.js
+++
@@ -1,7 +0,0 @@
-console.log('Redirecting to Rebble documentation.');
-document.body.innerHTML = 'Redirecting...';
-var loc = window.location.href,
-    split_loc = loc.split('/');
-split_loc.splice(2, 0, 'developer.rebble.io');
-split_loc.push('index.html');
-window.location.href = split_loc.join('/');
