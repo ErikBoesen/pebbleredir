a03f55a22f226d690ac2310769fbbc3da0903bcd
ErikBoesen <me@erikboesen.com>
2018-11-08 22:32:43 -0500

    Remove cruft from background script

--- background.js
+++ background.js
@@ -1,37 +1,13 @@
 chrome.webRequest.onBeforeRequest.addListener(function(frame) {
     console.log(frame);  // debug
-    return {cancel: true};
-    return {redirectUrl: "https://github.com/"};
-    chrome.tabs.update({url: 'https://google.com'});
+    return { redirectUrl: "" };
 },
 {
     urls: [
         "*://developer.getpebble.com/*",
-        "*://developer.pebble.com/*",
-        "*://erikboesen.com/*"
+        "*://developer.pebble.com/*"
     ],
     types: ["main_frame"]
 },
 ["blocking"]
 );
-/*const filter = {
-  urls: [
-    '*://news.ycombinator.com/*',
-  ],
-}
-
-const webRequestFlags = [
-  'blocking',
-];
-window.chrome.webRequest.onBeforeRequest.addListener(
-  page => {
-    console.log('page blocked - ' + page.url);
-
-    return {
-      cancel: true,
-    };
-  },
-  filter,
-  webRequestFlags,
-);
-*/
