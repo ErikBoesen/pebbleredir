3db1dd1b2bb4e92000eb9922f06bf8e8c0bb4284
ErikBoesen <me@erikboesen.com>
2018-11-08 22:34:33 -0500

    Write url rewriting logic

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 chrome.webRequest.onBeforeRequest.addListener(function(frame) {
     console.log(frame);  // debug
-    return { redirectUrl: "" };
+    var split_url = frame.url.split('/');
+    split_url.splice(2, 0, 'developer.rebble.io');
+    split_url.push('index.html');
+    return { redirectUrl: split_url.join('/') };
 },
 {
     urls: [
